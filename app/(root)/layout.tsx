cd3f1c7d2ec494b96103a192ec2f6656bf28aa00
SmartVar <130911742+SmartVar@users.noreply.github.com>
2024-01-04 22:15:12 +0530

    config

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable spaced-comment */
+//@ts-ignore
+
 import LeftSidebar from '@/components/shared/LeftSidebar'
 import Navbar from '@/components/shared/navbar/Navbar'
 import React from 'react'
@@ -6,14 +9,14 @@ import LeftSidebarDash from '@/components/shared/dashboard/LeftSidebarDash'
 
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const path='/';
+  // const path=``;
   
   return (
     <main className="background-light850_dark100 relative">
       <Navbar />
       <div className="flex">
-        {path==='/dashboard' ? <LeftSidebarDash />: <LeftSidebar />}
-
+        {/* {path==='/dashboard' ? <LeftSidebarDash />: <LeftSidebar />} */}
+        <LeftSidebar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">
             {children}
