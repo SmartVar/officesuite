73238a58289cde8fed7e549d5fab83d87259f606
SmartVar <130911742+SmartVar@users.noreply.github.com>
2024-01-04 22:49:03 +0530

    noting and drafting compoenent error fix

--- components/shared/dashboard/NotingDrafting.tsx
+++ components/shared/dashboard/NotingDrafting.tsx
@@ -8,16 +8,16 @@ interface NotingDraftingProps {
   createdAt: Date;
 }
 
-const templates = [
-  {
-    _id: '1',
-    type: 'History',
-    category: 'Report',
-    description: 'This is in connection to R.O. letter at 294/C, in this regards, the report in respect of Matheran PO and Holiday Home repairing is still not received from Navi Mumbai Division. Therefore, draft reminder letter is put up at 230/C, if agreed, for approval please.\n\nPut up for perusal and further orders please.',
-    createdAt: new Date('2023-09-01T12:00:00.000Z'),
-  },
+// const templates = [
+//   {
+//     _id: '1',
+//     type: 'History',
+//     category: 'Report',
+//     description: 'This is in connection to R.O. letter at 294/C, in this regards, the report in respect of Matheran PO and Holiday Home repairing is still not received from Navi Mumbai Division. Therefore, draft reminder letter is put up at 230/C, if agreed, for approval please.\n\nPut up for perusal and further orders please.',
+//     createdAt: new Date('2023-09-01T12:00:00.000Z'),
+//   },
    
-];
+// ];
 
 
 
@@ -30,7 +30,7 @@ const NotingDrafting = ({
 }: NotingDraftingProps) => {
   return (
     <div className="mt-11 flex justify-between gap-5 
-    max-sm:flex-col sm:items-center">{templates.category}</div>
+    max-sm:flex-col sm:items-center">category</div>
   )
 }
 
