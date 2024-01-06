7e92c22a5d64ff140bca4812b05dfa333d2d216b
SmartVar <130911742+SmartVar@users.noreply.github.com>
2024-01-06 23:59:07 +0530

    Answer and Question details page completed with Forms and other required components

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -1,22 +1,32 @@
 import Answer from '@/components/forms/Answer';
+import AllAnswers from '@/components/shared/AllAnswers';
 import Metric from '@/components/shared/Metric';
 import ParseHTML from '@/components/shared/ParseHTML';
 import RenderTag from '@/components/shared/RenderTag';
+import Votes from '@/components/shared/Votes';
 import { getQuestionById } from '@/lib/actions/question.action';
+import { getUserById } from '@/lib/actions/user.action';
 import { formatAndDivideNumber, getTimestamp } from '@/lib/utils';
+import { auth } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 
 const Page = async ({ params, searchParams }) => {
+  const { userId: clerkId } = auth();
+
+  let mongoUser;
+
+  if(clerkId) {
+    mongoUser = await getUserById({ userId: clerkId })
+  }
+
   const result = await getQuestionById({ questionId: params.id });
 
   return (
     <>
       <div className="flex-start w-full flex-col">
-        <div className="flex w-full flex-col-reverse 
-        justify-between gap-5 sm:flex-row sm:items-center 
-        sm:gap-2">
+        <div className="flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
           <Link href={`/profile/${result.author.clerkId}`}
           className="flex items-center justify-start gap-1"  >
             <Image 
@@ -27,17 +37,15 @@ const Page = async ({ params, searchParams }) => {
               height={22}
               alt="profile"
             />
-            <p className="paragraph-semibold 
-            text-dark300_light700">
+            <p className="paragraph-semibold text-dark300_light700">
               {result.author.name}
             </p>
           </Link>
           <div className="flex justify-end">
-            VOTING
+            <Votes />
           </div>
         </div>
-        <h2 className="h2-semibold text-dark200_light900 
-        mt-3.5 w-full text-left">
+        <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
           {result.title}
         </h2>
       </div>
@@ -79,7 +87,17 @@ const Page = async ({ params, searchParams }) => {
         ))}
       </div>
 
-      <Answer />
+      <AllAnswers 
+        questionId={result._id}
+        userId={JSON.stringify(mongoUser._id)}
+        totalAnswers={result.answers.length}
+      />
+
+      <Answer 
+        question={result.content}
+        questionId={JSON.stringify(result._id)}
+        authorId={JSON.stringify(mongoUser._id)}
+      />
     </>
   )
 }
--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -20,6 +20,7 @@ import { Badge } from '../ui/badge';
 import Image from 'next/image';
 import { createQuestion } from '@/lib/actions/question.action';
 import { useRouter, usePathname } from 'next/navigation';
+import { useTheme } from '@/context/ThemeProvider';
 
 const type:any = 'create'
 
@@ -28,6 +29,7 @@ interface Props {
 }
 
 const Question = ({ mongoUserId }: Props) => {
+  const { mode } = useTheme();
   const editorRef = useRef(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const router = useRouter();
@@ -108,20 +110,14 @@ const Question = ({ mongoUserId }: Props) => {
           name="title"
           render={({ field }) => (
             <FormItem className="flex w-full flex-col">
-              <FormLabel className="paragraph-semibold 
-              text-dark400_light800">Question Title 
-              <span className="text-primary-500">*</span></FormLabel>
+              <FormLabel className="paragraph-semibold text-dark400_light800">Question Title <span className="text-primary-500">*</span></FormLabel>
               <FormControl className="mt-3.5">
                 <Input 
-                className="no-focus paragraph-regular 
-                background-light900_dark300 light-border-2 
-                text-dark300_light700 min-h-[56px] border"
+                className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
                 {...field} />
               </FormControl>
-              <FormDescription className="body-regular mt-2.5
-               text-light-500">
-                Be specific and imagine you&apos;re asking a question 
-                to another person.
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Be specific and imagine you&apos;re asking a question to another person.
               </FormDescription>
               <FormMessage className="text-red-500" />
             </FormItem>
@@ -155,7 +151,9 @@ const Question = ({ mongoUserId }: Props) => {
                   'undo redo | ' +
                   'codesample | bold italic forecolor | alignleft aligncenter |' +
                   'alignright alignjustify | bullist numlist',
-                  content_style: 'body { font-family:Inter,Book Antiqua; font-size:16px }'
+                  content_style: 'body { font-family:Inter; font-size:16px }',
+                  skin: mode === 'dark' ? 'oxide-dark' : 'oxide',
+                  content_css: mode === 'dark' ? 'dark' : 'light', 
                 }}
               />
               </FormControl>
@@ -183,10 +181,7 @@ const Question = ({ mongoUserId }: Props) => {
                 {field.value.length > 0 && (
                   <div className="flex-start mt-2.5 gap-2.5">
                     {field.value.map((tag: any) => (
-                      <Badge key={tag} className="subtle-medium 
-                      background-light800_dark300 text-light400_light500 
-                      flex items-center justify-center gap-2 rounded-md 
-                      border-none px-4 py-2 capitalize" 
+                      <Badge key={tag} className="subtle-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize" 
                       onClick={() => handleTagRemove(tag, field)}>
                         {tag}
                         <Image 
@@ -194,8 +189,7 @@ const Question = ({ mongoUserId }: Props) => {
                           alt="Close icon"
                           width={12}
                           height={12}
-                          className="cursor-pointer object-contain
-                          invert-0 dark:invert"
+                          className="cursor-pointer object-contain invert-0 dark:invert"
                         />
                       </Badge>
                     ))}
@@ -210,8 +204,7 @@ const Question = ({ mongoUserId }: Props) => {
             </FormItem>
           )}
         />
-        <Button type="submit" className="primary-gradient w-fit
-         !text-light-900" disabled={isSubmitting}>
+        <Button type="submit" className="primary-gradient w-fit !text-light-900" disabled={isSubmitting}>
           {isSubmitting ? (
             <>
               {type === 'edit' ? 'Editing...' : 'Posting...' }
