3802cb4780dffbe0911c7c32e7defc8efce7e6ed
SmartVar <130911742+SmartVar@users.noreply.github.com>
2024-01-06 23:14:20 +0530

    Question Details page

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -3,7 +3,7 @@
 import Question from "@/database/question.model";
 import Tag from "@/database/tag.model";
 import { connectToDatabase } from "../mongoose"
-import { CreateQuestionParams, GetQuestionsParams } from "./shared.types";
+import { CreateQuestionParams, GetQuestionByIdParams, GetQuestionsParams } from "./shared.types";
 import User from "@/database/user.model";
 import { revalidatePath } from "next/cache";
 
@@ -45,11 +45,9 @@ export async function createQuestion(params: CreateQuestionParams) {
         { $setOnInsert: { name: tag }, $push: { question: question._id } },
         { upsert: true, new: true }
       )
-        // creating tag._id for new tag.
 
-      tagDocuments.push(existingTag._id); 
+      tagDocuments.push(existingTag._id);
     }
-    // enter new tag._id into relevant question
 
     await Question.findByIdAndUpdate(question._id, {
       $push: { tags: { $each: tagDocuments }}
@@ -59,10 +57,25 @@ export async function createQuestion(params: CreateQuestionParams) {
     
     // Increment author's reputation by +5 for creating a question
 
-    // revalidate path inorder to display question wihtout reloading
-    
     revalidatePath(path)
   } catch (error) {
     
+  }
+}
+
+export async function getQuestionById(params: GetQuestionByIdParams) {
+  try {
+    connectToDatabase();
+
+    const { questionId } = params;
+
+    const question = await Question.findById(questionId)
+      .populate({ path: 'tags', model: Tag, select: '_id name'})
+      .populate({ path: 'author', model: User, select: '_id clerkId name picture'})
+
+      return question;
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
 }
