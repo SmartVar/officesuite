1233a99b1333e47c221309e3c6b7deb5e9d1ad69
SmartVar <130911742+SmartVar@users.noreply.github.com>
2024-01-06 16:51:49 +0530

    auth error fix

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -15,17 +15,21 @@ export default async function Home() {
 
   return (
     <>
-      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+      <div className="flex w-full flex-col-reverse 
+      justify-between gap-4 sm:flex-row sm:items-center">
         <h1 className="h1-bold text-dark100_light900">All Questions</h1> 
 
-        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
-          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+        <Link href="/ask-question" className="flex 
+        justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] 
+          px-4 py-3 !text-light-900">
             Ask a Question
           </Button>
         </Link> 
       </div> 
 
-      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+      <div className="mt-11 flex justify-between 
+      gap-5 max-sm:flex-col sm:items-center">
         <LocalSearchbar 
           route="/"
           iconPosition="left"
@@ -60,7 +64,10 @@ export default async function Home() {
           ))
           : <NoResult 
             title="There’s no question to show"
-            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+            description="Be the first to break the silence! 
+            🚀 Ask a Question and kickstart the discussion. 
+            our query could be the next big thing others learn from. 
+            Get involved! 💡"
             link="/ask-question"
             linkTitle="Ask a Question"
           />}
