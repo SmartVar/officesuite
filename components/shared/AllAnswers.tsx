a309f1ca583c5b037ab5b69a758a6a480439d895
SmartVar <130911742+SmartVar@users.noreply.github.com>
2024-01-07 13:49:26 +0530

    VoteUI-Upvotes-Downvotes done

--- components/shared/AllAnswers.tsx
+++ components/shared/AllAnswers.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { getTimestamp } from '@/lib/utils';
 import ParseHTML from './ParseHTML';
+import Votes from './Votes';
 
 interface Props {
   questionId: string;
@@ -64,7 +65,15 @@ const AllAnswers = async ({ questionId, userId, totalAnswers,
                   </div>
                 </Link>
                 <div className="flex justify-end">
-                  VOTING
+                <Votes 
+                    type="Answer"
+                    itemId={JSON.stringify(answer._id)}
+                    userId={JSON.stringify(userId)}
+                    upvotes={answer.upvotes.length}
+                    hasupVoted={answer.upvotes.includes(userId)}
+                    downvotes={answer.downvotes.length}
+                    hasdownVoted={answer.downvotes.includes(userId)}
+                  />
                 </div>
               </div>
 
