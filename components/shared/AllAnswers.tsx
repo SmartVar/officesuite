aa88036b9c4b5a0874620ec5aea52eecf208d029
SmartVar <130911742+SmartVar@users.noreply.github.com>
2024-01-07 12:49:27 +0530

    Answer features updates and remove the picture error

--- components/shared/AllAnswers.tsx
+++ components/shared/AllAnswers.tsx
@@ -15,7 +15,8 @@ interface Props {
   filter?: number;
 }
 
-const AllAnswers = async ({ questionId, userId, totalAnswers, page, filter }: Props) => {
+const AllAnswers = async ({ questionId, userId, totalAnswers, 
+  page, filter }: Props) => {
   const result = await getAnswers({
     questionId,
   })
@@ -23,30 +24,40 @@ const AllAnswers = async ({ questionId, userId, totalAnswers, page, filter }: Pr
   return (
     <div className="mt-11">
       <div className="flex items-center justify-between">
-        <h3 className="primary-text-gradient">{totalAnswers} Answers</h3>
+        <h3 className="primary-text-gradient">{totalAnswers} 
+        Answers</h3>
 
         <Filter filters={AnswerFilters}/>
       </div>
 
       <div>
         {result.answers.map((answer) => (
-          <article key={answer._id} className='light-border border-b py-10'>
+          <article key={answer._id} className='light-border 
+          border-b py-10'>
             <div className="flex items-center justify-between">
-              <div className="mb-8 flex flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
+              <div className="mb-8 flex flex-col-reverse 
+              justify-between gap-5 sm:flex-row sm:items-center 
+              sm:gap-2">
                 <Link href={`/profile/${answer.author.clerkId}`} className="flex flex-1 items-start gap-1 sm:items-center">
                   <Image
-                    src={answer.author.picture}
+                    // src={answer.author.picture}
+                    src="/assets/icons/clock.svg"
                     width={18}
                     height={18}
                     alt="profile"
-                    className="rounded-full object-cover max-sm:mt-0.5"
+                    className="rounded-full object-cover 
+                    max-sm:mt-0.5"
                   />
-                  <div className="flex flex-col sm:flex-row sm:items-center">
-                    <p className="body-semibold text-dark300_light700">
+                  <div className="flex flex-col 
+                  sm:flex-row sm:items-center">
+                    <p className="body-semibold 
+                    text-dark300_light700">
                       {answer.author.name}
                     </p>
 
-                    <p className="small-regular text-light400_light500 ml-0.5 mt-0.5 line-clamp-1">
+                    <p className="small-regular 
+                    text-light400_light500 ml-0.5 mt-0.5 
+                    line-clamp-1">
                       answered {" "}
                       {getTimestamp(answer.createdAt)}
                       </p>
