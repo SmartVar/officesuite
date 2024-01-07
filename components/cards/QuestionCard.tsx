94c1314da9f91aefe512545f0cdc7976eba2daee
SmartVar <130911742+SmartVar@users.noreply.github.com>
2024-01-07 22:58:19 +0530

    Profile page creation. Badge creation.

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -1,5 +1,3 @@
-// @ts-nocheck
-
 import Link from 'next/link';
 import React from 'react'
 import RenderTag from '../shared/RenderTag';
@@ -22,9 +20,11 @@ interface QuestionProps {
   views: number;
   answers: Array<object>;
   createdAt: Date;
+  clerkId?: string;
 }
 
 const QuestionCard = ({
+  clerkId,
   _id,
   title,
   tags,
@@ -36,16 +36,13 @@ const QuestionCard = ({
 }: QuestionProps) => {
   return (
     <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
-      <div className="flex flex-col-reverse items-start justify-between gap-5 
-      sm:flex-row">
+      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
         <div>
-          <span className="subtle-regular text-dark400_light700 
-          line-clamp-1 flex sm:hidden">
+          <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
             {getTimestamp(createdAt)}
           </span>
           <Link href={`/question/${_id}`}>
-            <h3 className="sm:h3-semibold base-semibold text-dark200_light900 
-            line-clamp-1 flex-1">
+            <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
               {title} 
             </h3>
           </Link>
@@ -62,7 +59,8 @@ const QuestionCard = ({
 
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
           <Metric 
-            imgUrl="/assets/icons/avatar.svg"
+            // imgUrl={author.picture}
+            imgUrl="/assets/icons/like.svg"
             alt="user"
             value={author.name}
             title={` - asked ${getTimestamp(createdAt)}`}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-
+ 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
@@ -52,3 +52,14 @@ export const formatAndDivideNumber = (num: number): string => {
     return num.toString();
   }
 };
+
+export const getJoinedDate = (date: Date): string => {
+  // Extract the month and year from the Date object
+  const month = date.toLocaleString('default', { month: 'long' });
+  const year = date.getFullYear();
+
+  // Create the joined date string (e.g., "September 2023")
+  const joinedDate = `${month} ${year}`;
+
+  return joinedDate;
+}
