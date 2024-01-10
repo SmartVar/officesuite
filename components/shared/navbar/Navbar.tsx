97d8b7af9865e7e32bcf558535bddde42dbdc8f5
SmartVar <130911742+SmartVar@users.noreply.github.com>
2024-01-10 22:47:39 +0530

    Global Search Component Done

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -8,9 +8,7 @@ import GlobalSearch from '../search/GlobalSearch'
 
 const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 
-    fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none 
-    sm:px-12">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
@@ -19,13 +17,10 @@ const Navbar = () => {
           alt="DevFlow"
         />
 
-        <p className="h2-bold font-spaceGrotesk text-dark-100 
-        dark:text-light-900 max-sm:hidden">Dev 
-        <span className="text-primary-500">Overflow</span>
-        </p>
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">Dev <span className="text-primary-500">Overflow</span></p>
       </Link>
 
-     <GlobalSearch />
+      <GlobalSearch />
 
       <div className="flex-between gap-5">
         <Theme />
