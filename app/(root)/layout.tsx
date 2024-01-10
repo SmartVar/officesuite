7397d8893b2b340f0390f1e9dbf382da61bcf887
SmartVar <130911742+SmartVar@users.noreply.github.com>
2024-01-10 23:07:41 +0530

    Loading Components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,22 +1,16 @@
-/* eslint-disable spaced-comment */
-//@ts-ignore
-
 import LeftSidebar from '@/components/shared/LeftSidebar'
-import Navbar from '@/components/shared/navbar/Navbar'
-import React from 'react'
 import RightSidebar from '@/components/shared/RightSidebar'
-// import LeftSidebarDash from '@/components/shared/dashboard/LeftSidebarDash'
-
+import Navbar from '@/components/shared/navbar/Navbar'
+import { Toaster } from '@/components/ui/toaster'
+import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  // const path=``;
-  
   return (
     <main className="background-light850_dark100 relative">
       <Navbar />
       <div className="flex">
-        {/* {path==='/dashboard' ? <LeftSidebarDash />: <LeftSidebar />} */}
         <LeftSidebar />
+
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">
             {children}
@@ -26,7 +20,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <RightSidebar />
       </div>
 
-      Toaster
+      <Toaster />
     </main>
   )
 }
